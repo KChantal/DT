e8542f8bb016d313a96ba5846ec28845da90dbeb
Kimberley Wysocki <kwysocki@MacBook-Pro.local>
2020-07-16 16:03:37 -0400

    set up test for checking city/country data

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,12 @@ const request = require('supertest');
 const server = require('../server/app');
 
 describe('Endpoint tests', () => {
+  describe('sending data about city and country', () => {
+    it('should send data from external APIs when given city & country name', () => {
+      // TO DO
+    });
+  });
+
   describe('handling unknown endpoint', () => {
     it('should respond with status code 400', (done) => {
       return request(server)
